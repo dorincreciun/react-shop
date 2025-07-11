53f5207054f456e0c60ba21bf7b8c3fd88daef3d
dorincreciun <creciundorin25@gmail.com>
2025-07-11 16:59:41 +0300

    Refactor `ErrorMessage` animations and enhance `ContactPage` form structure
    - Update `ErrorMessage` animations: adjust styles to slide in from the bottom for improved user feedback.
    - Redesign `ContactPage` form: add email input, refine password input with toggleable errors, and improve layout with hover effects.

--- src/pages/Contact/ui/Contact.tsx
+++ src/pages/Contact/ui/Contact.tsx
@@ -1,16 +1,37 @@
 import {PageTemplate} from "@/shared";
 import {Input} from "@/shared/ui/Input";
-import {EnvelopeIcon} from "@heroicons/react/16/solid";
+import {useState} from "react";
 
 export const ContactPage = () => {
+    const [showError, setShowError] = useState(false)
+
+    const message = [
+        "wdwdwdwevbr wadw wd",
+        "wdwdwdwd g ftn547"
+    ]
+
     return (
         <PageTemplate>
-           <Input
-               type={'password'}
-               toggleMask
-               iconStart={<EnvelopeIcon className="h-6 w-6" />}
-               errorMessages={['Wdnenvnn jenjrnbtrnjb wjnfj', 'esfgtnrt 4ergrt wr3wfv wfe']}
-           />
+
+            <div className={'max-w-[700px] mx-auto p-10 bg-slate-800/20 rounded-xl border border-slate-600/20 text-center group hover:bg-slate-700/20 transition-colors duration-300'}>
+                <div className={'space-y-4'}>
+                    {/* Email input */}
+                    <Input
+                        type={'email'}
+                        placeholder={'Email'}
+                    />
+
+                    {/* Password input */}
+                    <Input
+                        type="password"
+                        toggleMask
+                        errorMessages={showError ? message : undefined}
+                    />
+
+                    <button onClick={() => setShowError((prev) => !prev)}>Show Error</button>
+                </div>
+            </div>
+
         </PageTemplate>
     )
 }
--- src/shared/ui/Input/ui/components/ErrorMessage.tsx
+++ src/shared/ui/Input/ui/components/ErrorMessage.tsx
@@ -1,12 +1,12 @@
 export const ErrorMessage = ({messages}: { messages: string[] }) => {
     return (
-        <div className="mt-2 text-sm text-red-400 animate-in slide-in-from-top-2 duration-300 ease-out">
+        <div className="mt-2 text-sm text-red-400 animate-in slide-in-from-bottom-2 duration-300 ease-out">
             <ul className="space-y-1">
                 {
                     messages.map((message: string, index: number) => (
                         <li
                             key={index}
-                            className="animate-in slide-in-from-top-1 duration-300 ease-out"
+                            className="animate-in slide-in-from-bottom-1 duration-300 ease-out"
                             style={{animationDelay: `${index * 100}ms`}}
                         >
                             {message}
